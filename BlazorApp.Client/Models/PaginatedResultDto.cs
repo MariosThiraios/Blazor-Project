6a86213a18d632a175f1bb04cc823a88b87e6e26
Marios Thiraios <mariosthiraios@gmail.com>
2025-05-24 14:38:29 +0300

    Add customer management pages for CRUD operations

---
+++ BlazorApp.Client/Models/PaginatedResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Client.Models
+{
+    // PaginatedResultDto class for client-side deserialization
+    public class PaginatedResultDto<T>
+    {
+        public List<T> Items { get; set; } = new List<T>();
+        public int TotalCount { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+    }
+}
